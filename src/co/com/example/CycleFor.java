0bfc908b7bd162b68dcc5fe14d5f3a54f50c5e87
Bymarium <marianamejia.91619@gmail.com>
2022-12-14 21:42:21 -0500

    First commit

---
+++ src/co/com/example/CycleFor.java
@@ -0,0 +1,9 @@
+package co.com.example;
+
+public class CycleFor {
+    public static void main(String[] args) {
+        for (int counter = 0; counter < 3; counter++){
+            System.out.println("counter = " + counter);
+        }
+    }
+}
